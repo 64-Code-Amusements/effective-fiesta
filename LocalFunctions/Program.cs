399e4ebe28692b51352a972b2c52a7de6eae5fc9
waltiam <2963975+waltiam@users.noreply.github.com>
2022-02-02 14:51:38 +0000

    🚚 🔥 let's go with just a console app (KISS)

---
+++ LocalFunctions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace LocalFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+                .CreateLogger();
+
+            log.Verbose("A Verbose message");
+            log.Debug("A Debug message");
+            log.Information("A Information message");
+            log.Warning("A Warning message");
+            log.Error("A Error message");
+            log.Fatal("A Fatal message");
+        }
+    }
+}
